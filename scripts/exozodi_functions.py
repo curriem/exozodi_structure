correcting SNR calculation functions

scripts/exozodi_functions.py
@@ -304,6 +304,8 @@ def synthesize_images_ADI(im_dir, sci_plan_i, sci_plan_j, ref_plan_i, ref_plan_j
     ref_back_CR_out = ref_disk_CR_out
     
     if add_star:
+        sci_back_CR += sci_star_CR
+        ref_back_CR += ref_star_CR
         sci_back_CR_out += sci_star_CR_out
         ref_back_CR_out += ref_star_CR_out
     ###################################################
@@ -315,13 +317,14 @@ def synthesize_images_ADI(im_dir, sci_plan_i, sci_plan_j, ref_plan_i, ref_plan_j
     
     if planet_noise:
         tot_noise_CR += sci_plan_CR
+        tot_noise_CR += ref_plan_CR
 
         
-    tot_tint = target_SNR**2 * tot_noise_CR/sci_plan_CR**2 # s
+    tot_tint = target_SNR**2 * tot_noise_CR/(sci_plan_CR+ref_plan_CR)**2 # s
     
 
-    sci_tint = tot_tint/2
-    ref_tint = tot_tint/2
+    sci_tint = tot_tint 
+    ref_tint = tot_tint 
 
     
     if verbose:
@@ -335,6 +338,8 @@ def synthesize_images_ADI(im_dir, sci_plan_i, sci_plan_j, ref_plan_i, ref_plan_j
         print("Ref Star counts:", ref_star_CR*ref_tint)
         print("Ref Integration time:", ref_tint)
         
+        print("SNR calculation:", (sci_plan_CR*sci_tint +ref_plan_CR*ref_tint) / np.sqrt(sci_plan_CR*sci_tint +ref_plan_CR*ref_tint + sci_disk_CR*sci_tint +sci_star_CR*sci_tint + ref_disk_CR*ref_tint + ref_star_CR*ref_tint) )
+        assert False
     sci_planet_counts, ref_planet_counts = sci_plan_CR*sci_tint, ref_plan_CR*ref_tint
 
     science_image = (sci_plan_im + sci_disk_im + sci_star_im) * sci_tint
@@ -453,6 +458,8 @@ def synthesize_images_RDI(im_dir, sci_plan_i, sci_plan_j, zodis, aperture,
     sci_back_CR_out = sci_disk_CR_out
     
     if add_star:
+        sci_back_CR += sci_star_CR
+        ref_back_CR += ref_star_CR
         sci_back_CR_out += sci_star_CR_out
         ref_back_CR_out += ref_star_CR_out
     ###################################################
@@ -469,8 +476,8 @@ def synthesize_images_RDI(im_dir, sci_plan_i, sci_plan_j, zodis, aperture,
     tot_tint = target_SNR**2 * tot_noise_CR/sci_plan_CR**2 # s
     
 
-    sci_tint = tot_tint/2
-    ref_tint = tot_tint/2
+    sci_tint = tot_tint
+    ref_tint = tot_tint
 
     
     if verbose:
@@ -483,6 +490,9 @@ def synthesize_images_RDI(im_dir, sci_plan_i, sci_plan_j, zodis, aperture,
         #print("Ref Disk counts:", ref_disk_CR*ref_tint)
         print("Ref Star counts:", ref_star_CR*ref_tint)
         print("Ref Integration time:", ref_tint)
+        
+        print("SNR calculation:", (sci_plan_CR*sci_tint) / np.sqrt(sci_plan_CR*sci_tint + sci_disk_CR*sci_tint +sci_star_CR*sci_tint + ref_star_CR*ref_tint) )
+        assert False
         
     sci_planet_counts = sci_plan_CR*sci_tint
 

scripts/run_SNRs.py
@@ -19,10 +19,11 @@ try:
     DI = str(sys.argv[2])
     noise_region = str(sys.argv[3])
 except IndexError:
-    print("WARNING: NO TELE, DI, NOISE REGION SPECIFIED. USING LUVA, ADI, CIRCLE.")
-    tele = "LUVB"
-    DI = "RDI"
-    noise_region = "wedge"
+    
+    tele = "LUVA"
+    DI = "ADI"
+    noise_region = "circle"
+    print("WARNING: NO TELE, DI, NOISE REGION SPECIFIED. USING {}, {}, {}.".format(tele, DI, noise_region))
 
 matched_filter_dir = "../matched_filter_library/"
 
@@ -416,7 +417,7 @@ def process(config):
     median_measured_signal_after_hipass = np.median(measured_signal_after_hipass_arr)
 
     
-    verbose = False
+    verbose = True
     if verbose:
         print("Median SNR before hipass:", median_SNR_before_hipass)
         print("Median SNR after hipass:", median_SNR_after_hipass)
@@ -441,12 +442,12 @@ def process(config):
     return return_arr
 
 
-parallel = True
+parallel = False
 
 # sequential runs
 if parallel == False:
     data = []
-
+    configs = [([1, 10, "00", "1", "uniform"])]
     for config in configs:
         
         data_arr  = process(config)