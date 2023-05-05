8d3070976ad4912f2f31b9cd379b995cc02541a4
MILES CURRIE <mcurr@klone-login01.hyak.local>
2023-05-04 17:00:48 -0700

    klone

--- scripts/run_SNRs.py
+++ scripts/run_SNRs.py
@@ -342,7 +342,7 @@ def process(config):
     std_noise_after_hipass_out = np.nanstd(measured_noise_after_hipass_out_arr, ddof=1)
 
     
-    verbose = True
+    verbose = False
     if verbose:
         print("Median SNR before hipass:", median_SNR_before_hipass)
         print("Median SNR after hipass:", median_SNR_after_hipass)
@@ -382,7 +382,7 @@ def process(config):
     return return_arr
 
 
-parallel = False
+parallel = True
 
 # sequential runs
 if parallel == False:
